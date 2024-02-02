07c4177505cb37f5e8abe7c07a915911db2dfbc1
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-02 17:33:23 +0100

    Create ColorsTest.svelte

---
+++ src/lib/ui/ColorsTest.svelte
@@ -0,0 +1,12 @@
+<script>
+	let midnightColors = ['pitch', 'spider', 'metal', 'grease', 'crow'];
+</script>
+
+<div class="text-green-500 font-bold w-20 h-20 bg-silver-50">bg-silver-50</div>
+<div class="text-green-500 font-bold w-20 h-20 bg-silver-100">bg-silver-100</div>
+
+<hr />
+
+{#each midnightColors as color}
+	<div class={`text-green-500 font-bold w-20 h-20 bg-midnight-${color}`}>bg-midnight-{color}</div>
+{/each}
