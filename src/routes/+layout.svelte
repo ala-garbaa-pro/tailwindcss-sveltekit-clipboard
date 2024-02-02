36bd6ebbf8cd477c72005981f1c14d640b1b49ac
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-02 17:34:17 +0100

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '../app.css';
+	import Sidebar from '$lib/ui/Sidebar.svelte';
+	import { navbarState } from '$lib/stores/navbar';
+	import Header from '$lib/ui/Header.svelte';
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class={`${$navbarState === 'normal-opened' ? 'flex' : 'block'} `}>
+	{#if $navbarState === 'normal-opened'}
+		<Sidebar />
+	{/if}
+
+	<div class="flex flex-col w-full" in:fade>
+		<Header />
+		<main class="p-4">
+			<slot />
+		</main>
+	</div>
+</div>
